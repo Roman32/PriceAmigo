e82f936518fa7e9a138f6a94679a48ac8e432609
Roman <claprood.8@osu.edu>
2016-02-19 16:08:07 -0500

    start of different activities

--- app/src/main/java/cse5236/priceamigo/MainMenu.java
+++ app/src/main/java/cse5236/priceamigo/MainMenu.java
@@ -1,7 +1,10 @@
 package cse5236.priceamigo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button settButt = (Button)findViewById(R.id.settings_button);
+        settButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this,Settings.class));
+            }
+        });
     }
 }
