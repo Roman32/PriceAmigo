220faf25257380db88a05466c497d23298691387
Roman <claprood.8@osu.edu>
2016-03-04 16:34:25 -0500

    flixed screen rotation for main menu

--- app/src/main/java/cse5236/priceamigo/MainMenu.java
+++ app/src/main/java/cse5236/priceamigo/MainMenu.java
@@ -3,6 +3,7 @@ package cse5236.priceamigo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +12,7 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainMenu", "In on Create");
         setContentView(R.layout.activity_main_menu);
         Button scanButt = (Button)findViewById(R.id.settings_button);
         scanButt.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +35,41 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(new Intent(MainMenu.this,Scan_History.class));
             }
         });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("MainMenu", "In on Start");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("MainMenu", "In on Resume");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("MainMenu", "In on Stop");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("MainMenu", "In on Restart");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("MainMenu", "In on Destroy");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("MainMenu", "In on Pause");
     }
 }
