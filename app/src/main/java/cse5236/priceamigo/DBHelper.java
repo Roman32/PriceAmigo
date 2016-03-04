02e0396df38ca933672bd18b9c9d115bdad6d802
Roman <claprood.8@osu.edu>
2016-03-04 15:19:00 -0500

    pulling

--- app/src/main/java/cse5236/priceamigo/DBHelper.java
+++ app/src/main/java/cse5236/priceamigo/DBHelper.java
@@ -12,5 +12,26 @@ import android.database.sqlite.SQLiteDatabase;
 /**
  * Created by Roman on 2/26/2016.
  */
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "PriceAmigo.db";
+    public DBHelper(Context context){
+        super(context,DATABASE_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table items " + "(id INTEGER AUTOINCREMENT PRIMARY KEY NOT NULL,upc TEXT NOT NULL,name TEXT,supplier TEXT,price TEXT NOT NULL,address TEXT,timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
+
+    public void insertItem(String upc, String name, String store, String price, String address){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+    }
 }
