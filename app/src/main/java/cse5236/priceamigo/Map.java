854f1718e1e05815aa1c5fa15a8c23e4a769aa41
Kelsey McHenry <mchenry.56@osu.edu>
2016-03-24 03:14:13 -0400

    scan goes to search results, hardcoded for now, and map button should work but is experiencing a weird bug

--- app/src/main/java/cse5236/priceamigo/Map.java
+++ app/src/main/java/cse5236/priceamigo/Map.java
@@ -1,11 +1,9 @@
 package cse5236.priceamigo;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class Map extends AppCompatActivity {
 
@@ -13,17 +11,9 @@ public class Map extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String url  = getIntent().getStringExtra("url");
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
-
 }
