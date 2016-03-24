0f827a643b82c513aa8126af9ee90274e67a379c
Kelsey McHenry <mchenry.56@osu.edu>
2016-03-24 18:13:19 -0400

    scan goes to search results, hardcoded for now, and map button should work but is experiencing a weird bug

--- app/src/main/java/cse5236/priceamigo/SearchResult.java
+++ app/src/main/java/cse5236/priceamigo/SearchResult.java
@@ -1,6 +1,7 @@
 package cse5236.priceamigo;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
@@ -46,25 +47,30 @@ public class SearchResult extends Activity {
             @Override
             public void onClick(View v) {
                 double x, y;
-                //TODO ERROR CHECK FOR NULL
-                Location location = getLastKnownLocation();
-                x = location.getLatitude();
-                y = location.getLongitude();
+                try {
+                    Location location = getLastKnownLocation();
+                    x = location.getLatitude();
+                    y = location.getLongitude();
 
-                Display display = getWindowManager().getDefaultDisplay();
-                Point size = new Point();
-                display.getSize(size);
-                int width = size.x;
-                //TODO get diameter remove hardcoded 4
-                int radius = 4; //= get diameter from settings
+                    Display display = getWindowManager().getDefaultDisplay();
+                    Point size = new Point();
+                    display.getSize(size);
+                    int width = size.x;
+                    //TODO get diameter remove hardcoded 4
+                    int radius = 4; //= get diameter from settings
 
-                int zoom = calculateZoomLevel(width, radius);
+                    int zoom = calculateZoomLevel(width, radius);
 
-                String url = "www.google.com/maps/search/" + getIntent().getStringExtra("store") + "/@" + x + "," + y + "," + zoom + "z";
+                    String url = "https://www.google.com/maps/search/" + getIntent().getStringExtra("store") + "/@" + x + "," + y + "," + zoom + "z";
 
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }catch (NullPointerException e){
+                    e.printStackTrace();
+                }catch (ActivityNotFoundException e){
+                    e.printStackTrace();
+                }
             }
         });
 
