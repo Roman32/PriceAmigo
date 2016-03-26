56059779866cf78adeea20493c1bbf88e1d4a687
Roman <claprood.8@osu.edu>
2016-03-25 22:35:36 -0400

    pulling changes from friday

--- app/src/main/java/cse5236/priceamigo/Scan.java
+++ app/src/main/java/cse5236/priceamigo/Scan.java
@@ -4,6 +4,7 @@ package cse5236.priceamigo;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
@@ -15,9 +16,10 @@ public class Scan extends AppCompatActivity {
     String upc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
-
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
         integrator.setPrompt("Scan a barcode");
@@ -28,6 +30,7 @@ public class Scan extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        WebScrape scraper = new WebScrape();
         if(result != null) {
             if(result.getContents() == null) {
                 //If scan is cancelled displays cancelled toast
@@ -50,15 +53,26 @@ public class Scan extends AppCompatActivity {
                     i.putExtra("store", <store here as String>);
                     startActivity(i);
                  */
+                //String name = scraper.getItemName(upc);
+                //Toast.makeText(this,name,Toast.LENGTH_LONG).show();
+                String price = scraper.scrapeWallyWorld(upc);
+                Intent i = new Intent(Scan.this,SearchResult.class);
+                i.putExtra("name","name");
+                i.putExtra("upc",upc);
+                i.putExtra("price",price);
+                i.putExtra("store", "Wally");
+                startActivity(i);
 
                 //hard coded testing lines
                 //TODO delete later
+                /*
                 Intent i = new Intent(Scan.this,SearchResult.class);
                 i.putExtra("name", "Android Programming: The Big Nerd Ranch Guide");
                 i.putExtra("upc", "9780134171456");
                 i.putExtra("price", "$27.30");
                 i.putExtra("store", "Walmart");
                 startActivity(i);
+                */
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
